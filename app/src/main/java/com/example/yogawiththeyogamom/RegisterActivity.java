f791b72f406e041b927a9d27d50e9ae11bb27259
Gabor <rabai.gabortamas@gmail.com>
2023-04-26 16:24:24 +0200

    RegisterActivity, on metódusok

--- app/src/main/java/com/example/yogawiththeyogamom/RegisterActivity.java
+++ app/src/main/java/com/example/yogawiththeyogamom/RegisterActivity.java
@@ -2,13 +2,95 @@ package com.example.yogawiththeyogamom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    private static final String PREF_KEY = RegisterActivity.class.getPackage().toString();
+    EditText userNameEditText;
+    EditText userEmailEditText;
+    EditText passwordEditText;
+    EditText pwConfirmEditText;
 
+    private SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
+
+        if(secret_key != 5){
+            finish();
+        }
+        userNameEditText = findViewById(R.id.userNameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        userEmailEditText = findViewById(R.id.userEmailEditText);
+        pwConfirmEditText = findViewById(R.id.pwConfirmEditText);
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password","");
+
+        userNameEditText.setText(userName);
+        passwordEditText.setText(password);
+        pwConfirmEditText.setText(password);
+
+        Log.i(LOG_TAG, "onCreate");
+    }
+
+    public void register(View view) {
+        String userName = userNameEditText.getText().toString();
+        String email = userEmailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String pwConfirm = pwConfirmEditText.getText().toString();
+        if (!password.equals(pwConfirm)){
+            Log.e(LOG_TAG, "A két jelszó nem egyezik meg!");
+            return;
+        }
+        Log.i(LOG_TAG, "Regisztrált: "+ userName+", e-mail cím"+email);
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
     }
 }
